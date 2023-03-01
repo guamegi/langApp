c0d8739a3f70b621125a5d6e898ca7e6b578163a
Minha Kwon <namisoft@naver.com>
2023-03-02 03:26:04 +0900

    first animation

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Animated, TouchableOpacity, View } from "react-native";
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(Animated.createAnimatedComponent(View))`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+`;
+// const AnimatedBox = Animated.createAnimatedComponent(Box);
+
 export default function App() {
+  const Y = new Animated.Value(0);
+  const moveUp = () => {
+    Animated.spring(Y, {
+      toValue: 200,
+      bounciness: 20,
+      useNativeDriver: true,
+    }).start();
+  };
+  Y.addListener(() => console.log(Y));
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <TouchableOpacity onPress={moveUp}>
+        <Box
+          style={{
+            transform: [{ translateY: Y }],
+          }}
+        />
+      </TouchableOpacity>
+    </Container>
   );
 }
