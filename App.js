3085ffc68417288fa3dc498cf1d3aa88b494700c
Minha Kwon <namisoft@naver.com>
2023-03-02 10:42:29 +0900

    more interpolations, ValueXY

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, { useRef, useState } from "react";
-import { Animated, Pressable, TouchableOpacity, View } from "react-native";
+import {
+  Animated,
+  Dimensions,
+  Pressable,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import styled from "styled-components/native";
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.View`
   flex: 1;
@@ -16,27 +24,73 @@ const Box = styled(Animated.createAnimatedComponent(View))`
 // const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const Y_POSITION = useRef(new Animated.Value(0)).current;
-  const [up, setUp] = useState(false);
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    })
+  ).current;
+  // const [up, setUp] = useState(false);
 
-  const toggleUp = () => setUp((prev) => !prev);
-
-  const moveUp = () => {
-    Animated.timing(Y_POSITION, {
-      toValue: up ? 300 : -300,
-      useNativeDriver: true,
-      duration: 500,
-    }).start(toggleUp);
-  };
-
-  const opacity = Y_POSITION.interpolate({
-    inputRange: [-300, 0, 300],
-    outputRange: [1, 0.5, 1],
+  // const toggleUp = () => setUp((prev) => !prev);
+  const topLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
   });
 
-  const borderRadius = Y_POSITION.interpolate({
+  const moveUp = () => {
+    // Animated.timing(POSITION, {
+    //   toValue: up ? 300 : -300,
+    //   useNativeDriver: false,
+    //   duration: 500,
+    // }).start(toggleUp);
+    Animated.loop(
+      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
+    ).start();
+  };
+
+  // const opacity = POSITION.interpolate({
+  //   inputRange: [-300, 0, 300],
+  //   outputRange: [1, 0.5, 1],
+  // });
+
+  // const rotation = POSITION.y.interpolate({
+  //   inputRange: [-300, 300],
+  //   outputRange: ["-360deg", "360deg"],
+  // });
+
+  const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
+  });
+
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
   });
   // Y.addListener(() => console.log(Y));
   return (
@@ -44,9 +98,9 @@ export default function App() {
       <Pressable onPress={moveUp}>
         <Box
           style={{
-            opacity,
+            backgroundColor: bgColor,
             borderRadius,
-            transform: [{ translateY: Y_POSITION }],
+            transform: [...POSITION.getTranslateTransform()],
           }}
         />
       </Pressable>
